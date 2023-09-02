dc9ee4591045e4fcf829f290a3118e9ff32355d4
Power Mostafa <powermostafa50015@gmail.com>
2023-09-02 16:31:30 +0300

    add db and enhance code

--- server/src/app.js
+++ server/src/app.js
@@ -3,19 +3,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
+const { sequelize } = require("./models");
+const config = require("./config/config");
 
 const app = express();
-const port = process.env.port || 8051;
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("combined"));
 
-app.post("/register", (req, res) => {
-  res.send({
-    message: "User Data " + req.body.email,
-  });
-});
+require("./route")(app);
 
-app.listen(port, () => {
-  console.log(`Local:   http://localhost:${port}/`);
+sequelize.sync().then(() => {
+  app.listen(config.port, () => {
+    console.log(`Local:   http://localhost:${config.port}/`);
+  });
 });
